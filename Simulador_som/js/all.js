6932c8bacbc67b5302ee61aa50b3f826c6f1e344
nikolasguimaraes <92951561+niqguimaraes@users.noreply.github.com>
2022-02-02 15:42:33 -0300

    Create all.js

---
+++ Simulador_som/js/all.js
@@ -0,0 +1,33 @@
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]; //Aqui o sistema pega a classe na posição 1 do array, ou seja, a classe tecla_<nome>
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio); //Passa o parametro de id do audio para a função id=som_<nome>
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
